1f298e8d52f6d2111100a9ec6d6a2aeb9c5d1f18
Aryan Dwivedi <aryan.735921@gmail.comm>
2023-07-27 19:09:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+async function logSpells(){
+    try{
+        const response=await fetch("https://harry-potter-api-en.onrender.com/spells");
+        const spells=await response.json();
+        console.log(spells);
+
+    }catch(e){
+        console.log(e);
+    }
+}
+
+logSpells();
